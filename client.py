4cafbc98d84162c699bbcef78c6b9c300f8c8094
Sam Bolgert <sam@livelovely.com>
2012-06-22 11:37:25 -0700

    initial commit

---
+++ client.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, json
+import os
+import urllib2
+from urllib import urlencode
+import socket
+
+app = Flask(__name__)
+port = 1337
+
+
+@app.route('/heartbeat', methods=['POST'])
+def heartbeat():
+	return json.dumps({'alive':True})
+
+@app.route('/event', methods=['POST'])
+def event():
+	print request.form['event']
+	if request.form['event']['sound'] == 'nuts':
+		os.system("afplay ./assets/lovenuts.mp3")
+	elif request.form['event']['sound'] == 'pizza':
+		os.system("afplay ./assets/pizzaaaaaaa.mp3")
+	elif request.form['event']['sound'] == 'bagel':
+		os.system("afplay ./assets/pizzabagel.mp3")
+	elif request.form['event']['sound'] == 'steemer':
+		os.system("afplay ./assets/steemer.mp3")
+
+	return 'ok'
+
+#register client
+ip = socket.gethostbyname(socket.gethostname())
+data = urlencode({'ip': ip})
+register = urllib2.Request("192.168.1.119:1337/register")
+register.add_data(data)
+urllib2.urlopen(register).read()
+app.run(port=port)
---
+++ server.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, json
+import urllib2
+import time
+import threading
+
+app = Flask(__name__)
+port = 1337
+
+
+listeners = []
+
+@app.route('/register', methods=['POST'])
+def register():
+	if request.form.has_key('ip'):
+		listeners.append(request.form['ip'])
+
+@app.route('/email', methods=['POST'])
+def email():
+	if request.form.has_key('event'):
+		event = {'event':{'name':'something', 'sound':request.form['event']['sound']}}
+		send_update(event)
+
+def check_alive():
+	for listener in listeners:
+		request = urllib2.Request(listener+":"+str(port)+"/heartbeat")
+		try:
+			urllib2.urlopen(request).read()
+		except Exception as ex:
+			listeners.remove(listener)
+
+def send_update(event):
+	for listener in listeners:
+		request = urllib2.Request(listener+":"+str(port)+"/event")
+		request.add_data(event)
+		try:
+			urllib2.urlopen(request).read()
+		except Exception as ex:
+			print ex
+
+class Timer(threading.Thread):
+	def __init__(self, seconds):
+	 	self.runTime = seconds
+ 		threading.Thread.__init__(self)
+ 	def run(self):
+	 	time.sleep(self.runTime)
+		check_alive()
+		self.run()
+
+Timer(60*5)
+app.run(port=port)
+
+
